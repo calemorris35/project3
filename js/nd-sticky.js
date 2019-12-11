b3083d6a59f6462b01995049289faa1b338d8d87
Cale Morris <calemorris@cales-mbp.dhcp.nd.edu>
2019-12-11 11:42:18 -0500

    update

---
+++ js/nd-sticky.js
@@ -0,0 +1,35 @@
+/*!
+ * Sticky-kit
+ * A jQuery plugin for making smart sticky elements
+ *
+ * Source: http://leafo.net/sticky-kit/
+ */
+
+$("#intro_title").stick_in_parent({
+    // offset_top: 10,
+    // offset_left: 100
+});
+
+$("#when_title").stick_in_parent({
+     // offset_top: 10
+});
+
+$("#water_title").stick_in_parent({
+    // offset_top: 10
+});
+
+$("#soil_title").stick_in_parent({
+    // offset_top: 10
+});
+
+$("#shelter_title").stick_in_parent({
+    // offset_top: 10
+});
+
+$("#kale_title").stick_in_parent({
+    // offset_top: 10
+});
+
+$("#conclusion_title").stick_in_parent({
+    // offset_top: 10
+});
